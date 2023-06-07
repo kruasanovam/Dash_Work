92326d87c7c12df59f23a1026052c25eff1ef23e
Torsten Motzkus <tmotzkus@web.de>
2023-06-07 14:10:47 +0200

    Styling streamlit & upload of geojson files

--- notebooks/Lennart_Streamlit_test.py
+++ notebooks/Lennart_Streamlit_test.py
@@ -12,6 +12,11 @@ from streamlit_folium import folium_static
 
 st.set_page_config(layout="wide")
 
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+st.title("Open positions in Germany")
+
 st.sidebar.info(
     """
     Dash_Work Test for Streamlit>
@@ -31,7 +36,7 @@ st.sidebar.selectbox("Select Sector", options=[1,2])
 
 @st.cache_data()
 def get_data():
-    gdf = gpd.read_file("plz-5stellig.geojson")
+    gdf = gpd.read_file("/Users/tmotzkus/Downloads/landkreise_simplify200.geojson")
     return gdf
 
 gdf = get_data()
@@ -45,12 +50,14 @@ gdf = get_data()
 #     return m
 
 
-m = folium.Map(location=[51, 9], tiles="cartodbpositron", zoom_start=6)
+m = folium.Map(location=[51.1657, 10.4515], tiles="cartodbpositron", zoom_start=6)
 style = {"fill_color": "green", "opacity" : .5, "weight": 1,"color": "green", "fillOpacity": .5}
 gjson = folium.GeoJson(data=gdf, style_function=lambda x:style).add_to(m)
-popup=folium.GeoJsonPopup(fields=["einwohner"]).add_to(gjson)
+gjson.add_child(folium.features.GeoJsonTooltip(['GEN'],labels=False))
 
-st_folium(m, returned_objects=[])
+popup=folium.GeoJsonPopup(fields=["GEN"]).add_to(gjson)
+
+st_folium(m, returned_objects=[],  width=600, height=600)
 
 
 # def app():
